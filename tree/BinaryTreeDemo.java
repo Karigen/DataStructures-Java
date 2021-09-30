6a4797511d3b784ed145fee46135642ad93f82be
Karigen <1837633996@qq.com>
2021-09-30 22:36:43 +0800

    二叉树搜索

--- tree/BinaryTreeDemo.java
+++ tree/BinaryTreeDemo.java
@@ -20,6 +20,22 @@ package tree;
  *  4.3输出当前节点
  *  
  * 小结：看输出父节点的顺序，就确定是前序，中序还是后序
+ * 
+ * 前序查找思路
+ * 1.先判断当前节点的no是否等于要查找的
+ * 2.如果是相等，则返回当前节点
+ * 3.如果不等，则判断当前节点的左子节点是否为空，如果不空，则递归前序查找
+ * 4.如果左递归前序查找，找到节点，则返回，否则继续判断，当前节点的右子节点是否为空，如果不空，则继续向右递归前序查找
+ * 
+ * 中序遍历思路
+ * 1.判断当前节点的左子节点是否为空，如果不空，则递归中序查找
+ * 2.如果找到，则返回，如果没有找到，就和当前节点比较，如果是则返回当前节点，否则继续进行右递归的中序查找
+ * 3.如果右递归中序查找，找到就返回，否则返回null
+ * 
+ * 后续查找思路
+ * 1.判断当前节点的左子节点是否为空，如果不为空，则递归后续查找
+ * 2.如果找到，就返回，如果没有找到，就判断当前节点的右子节点是否为空，如果不为空，则右递归进行后序查找，如果找到，就返回
+ * 3.就和当前节点进行比较，如果是则返回，否则返回null
  */
 
 public class BinaryTreeDemo {
@@ -54,6 +70,42 @@ public class BinaryTreeDemo {
 	//测试
 	System.out.println("后序遍历");//2， 5， 4， 3， 1
 	binaryTree.postOrder();
+	
+	/*
+	
+	//前序遍历
+	//前序遍历的次数：4
+	System.out.println("前序遍历方式：");
+	HeroNode resNode=binaryTree.preOrderSearch(5);
+	if (resNode!=null) {
+	    System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
+	} else {
+	    System.out.printf("没有找到no=%d的英雄",5);
+	}
+	
+	
+	
+	//中序遍历查找
+	//中序遍历3次
+	System.out.println("中序遍历方式：");
+	HeroNode resNode=binaryTree.infixOrderSearch(5);
+	if (resNode!=null) {
+	    System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
+	} else {
+	    System.out.printf("没有找到no=%d的英雄",5);
+	}
+	
+	*/
+	
+	//后序遍历查找
+	//后序遍历查找的次数 2次
+	System.out.println("后序遍历方式：");
+	HeroNode resNode=binaryTree.postOrderSearch(2);
+	if (resNode!=null) {
+	    System.out.printf("找到了，信息为no=%d name=%s",resNode.getNo(),resNode.getName());
+	} else {
+	    System.out.printf("没有找到no=%d的英雄",2);
+	}
     }
 
 }
@@ -90,6 +142,33 @@ class BinaryTree {
 	    this.root.postOrder();
 	} else {
 	    System.out.println("二叉树为空，无法遍历");
+	}
+    }
+    
+    //前序遍历
+    public HeroNode preOrderSearch(int no) {
+	if (root!=null) {
+	    return root.preOrderSearch(no);
+	} else {
+	    return null;
+	}
+    }
+    
+    //中序遍历
+    public HeroNode infixOrderSearch(int no) {
+	if (root!=null) {
+	    return root.infixOrderSearch(no);
+	} else {
+	    return null;
+	}
+    }
+    
+    //后序遍历
+    public HeroNode postOrderSearch(int no) {
+	if (root!=null) {
+	    return root.postOrderSearch(no);
+	} else {
+	    return null;
 	}
     }
 }
@@ -186,5 +265,97 @@ class HeroNode {
 	}
 	
 	System.out.println(this);
+    }
+    
+    //前序遍历查找
+    /**
+     * 
+     * @param no 查找no
+     * @return 如果找到就返回该node，如果没有找到就返回null
+     */
+    public HeroNode preOrderSearch(int no) {
+	System.out.println("进入前序遍历");
+	
+	//先比较当前节点是不是
+	if (this.no==no) {
+	    return this;
+	}
+	
+	//1.则判断当前节点的左子节点是否为空，如果不空，则递归前序查找
+	//2.如果左递归前序查找，找到节点，则返回
+	HeroNode resNode=null;
+	if (this.left!=null) {
+	    resNode=this.left.preOrderSearch(no);
+	}
+	
+	if (resNode!=null) {//说明我们左子树找到
+	    return resNode;
+	}
+	
+	//1.左递归前序查找，找到节点，则返回，否则继续判断，
+	//2.当前节点的右子节点是否为空，如果不空，则继续向右递归前序查找
+	if (this.right!=null) {
+	    resNode=this.right.preOrderSearch(no);
+	}
+	
+	return resNode;
+    }
+    
+    //中序遍历查找
+    public HeroNode infixOrderSearch(int no) {
+	//判断当前节点的左子节点是否为空，如果不空，则递归中序查找
+	HeroNode resNode=null;
+	if (this.left!=null) {
+	    resNode=this.left.infixOrderSearch(no);
+	}
+	
+	if (resNode!=null) {
+	    return resNode;
+	}
+	
+	System.out.println("进入中序查找");
+	
+	//如果找到，则返回，如果没有找到，就和当前节点比较，如果是则返回当前节点
+	if (this.no==no) {
+	    return this;
+	}
+	
+	//否则继续进行右递归的中序查找
+	if (this.right!=null) {
+	    resNode=this.right.infixOrderSearch(no);
+	}
+	
+	return resNode;
+    }
+    
+    //后序遍历查找
+    public HeroNode postOrderSearch(int no) {
+	//判断当前节点的左子节点是否为空，如果不为空，则递归后续查找
+	HeroNode resNode=null;
+	if (this.left!=null) {
+	    resNode=this.left.postOrderSearch(no);
+	}
+	
+	if (resNode!=null) {//说明在左子树找到
+	    return resNode;
+	}
+	
+	//如果左子树没有找到，则向右子树递归进行后续遍历查找
+	if (this.right!=null) {
+	    resNode=this.right.postOrderSearch(no);
+	}
+	
+	if (resNode!=null) {
+	    return resNode;
+	}
+	
+	System.out.println("进入后序查找");
+	
+	//如果左右子树都没有找到，就比较当前而节点是不是
+	if (this.no==no) {
+	    return this;
+	}
+	
+	return resNode;
     }
 }
