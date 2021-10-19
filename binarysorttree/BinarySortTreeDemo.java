28c0d3cbc88421bd731b1d7ad185426bd57c2aa7
Karigen <1837633996@qq.com>
2021-10-19 20:17:05 +0800

    二叉排序树的删除

--- binarysorttree/BinarySortTreeDemo.java
+++ binarysorttree/BinarySortTreeDemo.java
@@ -1,5 +1,43 @@
 package binarysorttree;
 
+/*
+ * 二叉排序树--天然适合二分法--中序遍历为小->大顺序
+ * 对于二叉排序树的任何一个非叶子节点，要求左子结点的值比当前结点的值小，右子节点的值比当前结点的值大
+ * 特别说明：如果有相同的值，可以将该节点放在左子结点或右子节点
+ * 
+ * 删除二叉排序树结点
+ * 第一种情况：
+ * 删除叶子结点
+ * （1）需求先去找到要删除的结点 targetNode
+ * （2）找到targetNode的父结点parent
+ * （3）确定targetNode是parent的左子结点，还是右子节点
+ * （4）根据前面的情况对应删除
+ * 	左子结点：parent.left=null
+ * 	右子节点：parent.right=null
+ * 
+ * 第二种情况
+ * 删除只有一棵子树的结点
+ * （1）需求先去找到要删除的结点 targetNode
+ * （2）找到targetNode的父结点parent
+ * （3）确定targetNode的子结点是左子结点，还是右子节点
+ * （4）targetNode是parent的左子结点，还是右子节点
+ * （5）如果targetNode有左子结点
+ * 	1.如果targetNode是parent的左子结点：parent.left=targetNode.left
+ * 	2.如果targetNode是parent的右子结点：parent.right=targetNode.left
+ * （6）如果targetNode有右子结点
+ * 	1.如果targetNode是parent的左子结点：parent.left=targetNode.right
+ * 	2.如果targetNode是parent的右子结点：parent.right=targetNode.right
+ * 
+ * 第三种情况
+ * 删除有两棵子树的结点
+ * （1）需求先去找到要删除的结点 targetNode
+ * （2）找到targetNode的父结点parent
+ * （3）从targetNode的右子树找到最小的结点--叶子结点（从targetNode的左子树找到最大的结点）
+ * （4）用一个临时变量，将最小结点的值保存至temp
+ * （5）删除最小结点
+ * （6）targetNode.value=temp
+ */
+
 public class BinarySortTreeDemo {
 
     public static void main(String[] args) {
@@ -18,10 +56,15 @@ public class BinarySortTreeDemo {
 	
 	//测试一下删除叶子结点
 	binarySortTree.delNode(2);
-	//binarySortTree.delNode(5);
-	//binarySortTree.delNode(9);
-	//binarySortTree.delNode(12);
+	binarySortTree.delNode(5);
+	binarySortTree.delNode(9);
+	binarySortTree.delNode(12);
+	binarySortTree.delNode(7);
+	binarySortTree.delNode(3);
 	binarySortTree.delNode(1);
+	binarySortTree.delNode(10);
+	
+	System.out.println("root="+binarySortTree.getRoot());
 	
 	System.out.println("删除节点后");
 	binarySortTree.infixOrder();
@@ -33,6 +76,14 @@ public class BinarySortTreeDemo {
 class BinarySortTree{
     private Node root;
     
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
     //查找要删除的结点
     public Node search(int value) {
 	if (root==null) {
@@ -49,6 +100,28 @@ class BinarySortTree{
 	} else {
 	    return root.searchParent(value);
 	}
+    }
+    
+    //编写方法
+    //1.返回的是以node为根节点的二叉排序树的最小结点的值
+    //2.删除node为根节点的二叉排序树的最小结点
+    /**
+     * 
+     * @param node 传入的结点（当作二叉排序树的根节点）
+     * @return 返回的是以node为根节点的二叉排序树的最小结点的值
+     */
+    public int delRightTreeMin(Node node) {
+	Node target=node;
+	
+	//循环查找左子结点，就会找到最小值
+	while (target.left!=null) {
+	    target=target.left;
+	}
+	
+	//这时target就指向了最小结点
+	//删除最小结点
+	delNode(target.value);
+	return target.value;
     }
     
     //删除节点
@@ -80,6 +153,33 @@ class BinarySortTree{
 		    parent.left=null;
 		} else if (parent.right!=null&&parent.right.value==value) {//是右子节点
 		    parent.right=null;
+		}
+	    } else if (targetNode.left!=null&&targetNode.right!=null) {//删除有两棵子树的结点
+		int minVal=delRightTreeMin(targetNode.right);
+		targetNode.value=minVal;
+	    } else {//删除只有一棵子树的结点
+		if (targetNode.left!=null) {//如果要删除的结点有左子结点
+		    if (parent!=null) {
+			//如果targetNode是parent的左子结点
+			if (parent.left.value==value) {
+			    parent.left=targetNode.left;
+		    	} else {//targetNode是parent的右子结点
+		    	    parent.right=targetNode.left;
+		    	}
+		    } else {
+			root=targetNode.left;
+		    }
+		} else {//如果要删除的结点有右子结点
+		    if (parent!=null) {
+			//如果targetNode是parent的左子结点
+			if (parent.left.value==value) {
+			    parent.left=targetNode.right;
+			} else {//如果targetNode是parent的右子结点
+			    parent.right=targetNode.right;
+			}
+		    } else {
+			root=targetNode.right;
+		    }
 		}
 	    }
 	}
